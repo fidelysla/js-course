9b725da5b6f0f1bdf87e2579fe7f626239e1bf6d
Fidel Ysla <ysla2021@gmail.com>
2025-01-31 13:10:58 -0500

    dom exercises 83 alarma

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -24,6 +24,25 @@ export function digitalClock(clock, btnPlay, btnStop) {
     })
 }
 
-export function alarma() {
-    
+export function alarm(sound, btnPlay, btnStop) {
+
+    let alarmTempo;
+    const $alarm = d.createElement("audio")
+    $alarm.src = sound
+
+    d.addEventListener("click", e => {
+        if (e.target.matches(btnPlay)) {
+            alarmTempo = setTimeout(() => {
+                $alarm.play();
+            }, 500);
+            e.target.disabled = true
+        }
+
+        if (e.target.matches(btnStop)) {
+            clearTimeout(alarmTempo)
+            $alarm.pause();
+            $alarm.currentTime = 0;
+            d.querySelector(btnPlay).disabled = false;
+        }
+    })
 }
