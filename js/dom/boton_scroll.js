4f6ca0e9c53005a7e3d5af7382e3e02a5b8a9e04
Fidel Ysla <ysla2021@gmail.com>
2025-02-02 16:54:35 -0500

    dom exercises 88 dark light theme

--- js/dom/boton_scroll.js
+++ js/dom/boton_scroll.js
@@ -6,11 +6,17 @@ const d = document,
 export default function scrollTopButton(btn) {
     const $scrollBtn = d.querySelector(btn)
 
+    // console.log(d.documentElement.scrollTop);
+    
+    if (d.documentElement.scrollTop > 65) {
+        $scrollBtn.classList.remove("hidden")
+    }
+
     w.addEventListener("scroll", (e) => {
         // w.pageXOffset (deprecated)
         let scrollTop = d.documentElement.scrollTop
 
-        if (scrollTop > 68.87893127441) {
+        if (scrollTop > 65) {
             $scrollBtn.classList.remove("hidden")
         } else {
             $scrollBtn.classList.add("hidden")
--- js/index_dom.js
+++ js/index_dom.js
@@ -4,20 +4,22 @@ import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
     console.clear()
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
-    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
-    alarm("#alarm", "/assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma")
-    // countdown("countdown", "Feb 06, 2025 12:00:00", "Feliz Cumpleaños, Gael 🎂")
-    scrollTopButton(".scroll-top-btn")
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("#alarm", "/assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+    // countdown("countdown", "Feb 06, 2025 12:00:00", "Feliz Cumpleaños, Gael 🎂");
+    scrollTopButton(".scroll-top-btn");
+    darkTheme(".dark-theme-btn", "dark-mode")
 })
 
 d.addEventListener("keydown", (e) => {
     console.clear()
     shortcuts(e)
-    moveBall(e, ".ball", ".stage")
+    // moveBall(e, ".ball", ".stage")
 })
