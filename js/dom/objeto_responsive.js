2ead2ab34c09749079bebef9a201727f4e0d2df3
Fidel Ysla <ysla2021@gmail.com>
2025-02-02 19:35:50 -0500

    dom exercise 90 responsible responsive design mediaQuery yt map

---
+++ js/dom/objeto_responsive.js
@@ -0,0 +1,28 @@
+
+
+// responsible responsive design mediaQuery
+const d = document,
+    w = window;
+
+export default function responsiveMedia(id, mq, mobileContent, desktopContent) {
+
+    let breakpoint = w.matchMedia(mq);
+
+    console.log(breakpoint);
+    
+    const responsive = e => {
+        if (e.matches) {
+            d.getElementById(id).innerHTML = desktopContent;
+            // console.log("1024", e.matches);
+        } else {
+            d.getElementById(id).innerHTML = mobileContent;
+            // console.log("1024", e.matches);
+        }
+        console.log(e.matches);
+    };
+    
+    // breakpoint.addListener(responsive) // addListener (deprecated)
+    breakpoint.addEventListener("change", responsive);
+
+    responsive(breakpoint);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,26 +1,44 @@
-
-import scrollTopButton from "./dom/boton_scroll.js";
-import countdown from "./dom/cuenta_regresiva.js";
-import hamburgerMenu from "./dom/menu_hamburguesa.js";
-import { digitalClock, alarm } from "./dom/reloj.js";
-import { moveBall, shortcuts } from "./dom/teclado.js";
-import darkTheme from "./dom/tema_oscuro.js";
+import scrollTopButton from './dom/boton_scroll.js';
+import countdown from './dom/cuenta_regresiva.js';
+import hamburgerMenu from './dom/menu_hamburguesa.js';
+import responsiveMedia from './dom/objeto_responsive.js';
+import { digitalClock, alarm } from './dom/reloj.js';
+import { moveBall, shortcuts } from './dom/teclado.js';
+import darkTheme from './dom/tema_oscuro.js';
 
 const d = document;
 
-d.addEventListener("DOMContentLoaded", e => {
-    console.clear()
-    hamburgerMenu(".panel-btn", ".panel", ".menu a");
-    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-    alarm("#alarm", "/assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+d.addEventListener('DOMContentLoaded', (e) => {
+    console.clear();
+    hamburgerMenu('.panel-btn', '.panel', '.menu a');
+    digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
+    alarm(
+        '#alarm',
+        '/assets/alarma.mp3',
+        '#activar-alarma',
+        '#desactivar-alarma'
+    );
     // countdown("countdown", "Feb 06, 2025 12:00:00", "Feliz Cumpleaños, Gael 🎂");
-    scrollTopButton(".scroll-top-btn");
-})
+    scrollTopButton('.scroll-top-btn');
 
-d.addEventListener("keydown", (e) => {
-    console.clear()
-    shortcuts(e)
+    responsiveMedia(
+        'youtube',
+        '(min-width: 1024px)',
+        'Contenido Movil',
+        'Contenido Escritorio'
+    );
+    // responsiveMedia(
+    //     'gmaps',
+    //     '(min-width: 1024px)',
+    //     'Contenido Movil',
+    //     'Contenido Escritorio'
+    // );
+});
+
+d.addEventListener('keydown', (e) => {
+    console.clear();
+    shortcuts(e);
     // moveBall(e, ".ball", ".stage")
-})
+});
 
-darkTheme(".dark-theme-btn", "dark-mode")
+darkTheme('.dark-theme-btn', 'dark-mode');
